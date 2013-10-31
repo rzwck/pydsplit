7bbf808e80d5f1f711e0849f3b0dc17d783a25b6
Rizki Wicaksono <rizki.wicaksono@gmail.com>
2013-10-31 15:28:16 +0700

    Update pydsplit.py

--- pydsplit.py
+++ pydsplit.py
@@ -1,4 +1,29 @@
-pydsplit
-========
+import sys,os
 
-i made this simple lite version of dsplit.exe to detect antivirus signature matching
+# incsplit.py [startoffset] [blocksize] [filename]
+
+startoffset = int(sys.argv[1])
+blocksize = int(sys.argv[2])
+filename = sys.argv[3]
+
+size = os.path.getsize(filename)
+
+fh = open(filename, "rb")
+nameonly = os.path.splitext(filename)[0]
+extonly = os.path.splitext(filename)[1]
+
+data = ""
+if startoffset > 0:
+	data = fh.read(startoffset)
+
+cursor = fh.tell()
+while cursor < size:
+	data += fh.read(blocksize)
+	newname = nameonly + "_" + str(len(data)) + extonly
+	cursor = fh.tell()
+	
+	newfh = open(newname, "wb")
+	newfh.write(data)
+	newfh.close()
+
+fh.close()
